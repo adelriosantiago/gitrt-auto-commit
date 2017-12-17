2ae007384c74eb027cf7e3ae631d94e21be2f7eb
Alejandro del Río <adelriosantiago@gmail.com>
2017-12-16 22:29:22 -0600

    Working autocommit

--- index.js
+++ index.js
@@ -2,10 +2,62 @@
 
 'use strict';
 
-var fs = require('fs');
+var fs = require('fs'),
+  path = require('path'),
+  nodegit = require("nodegit");
+
+//Default options
+var defaultOptions = { path: "./example-repo/", interval: 1000, author: {}, commiter: {}, message: "GitRT auto commit" };
+
+function options(opts) {
+  defaultOptions = _.merge(defaultOptions, opts);
+}
 
 function run() {
-  console.log("test");
+  nodegit.Repository.open(path.join(__dirname, defaultOptions.path)).then(function(repo) {
+    var index, oid;
+    
+    console.log("Repository open", repo);
+    
+    var statusCheck = function() {
+      setInterval(function() {
+        console.log("c interval");
+        
+        repo.getStatus().then(function(status) {
+          
+          if (status.length <= 0) return;
+          
+          return repo.refreshIndex();
+        }).then(function(_index) {
+          index = _index;
+          
+          return index.addAll();
+        }).then(function() {
+          
+          return index.write();
+        }).then(function() {
+          
+          return index.writeTree();
+        }).then(function(_oid) {
+          oid = _oid;
+          
+          return nodegit.Reference.nameToId(repo, "HEAD");
+        }).then(function(head) {
+          
+          return repo.getCommit(head);
+        }).then(function(parent) {
+          var author = nodegit.Signature.default(repo),
+            committer = nodegit.Signature.now("GitRT", "GitRT");
+            
+          return repo.createCommit("HEAD", author, committer, defaultOptions.message, oid, [parent]);
+        })
+      }, defaultOptions.interval);
+    }
+    
+    statusCheck();
+  }).catch(function(e) {
+    console.log("Error opening repo", e);
+  });
 }
 
 module.exports = {
