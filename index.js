434587e699102a5b8e2cf0aff77bb29adc7b0424
Alejandro del Río <adelriosantiago@gmail.com>
2020-08-08 01:16:21 -0500

    gitrt-auto-commit on Sat Aug 08 2020 01:16:20 GMT-0500 (Central Daylight Time)

--- index.js
+++ index.js
@@ -2,3 +2,30 @@
 "use strict"
 
 const path = require("path")
+const simpleGit = require("simple-git")
+
+let settings = {
+  baseDir: path.join(process.cwd(), "./"),
+  binary: "git",
+  maxConcurrentProcesses: 6,
+  timeout: 1000,
+}
+
+const git = simpleGit(settings)
+
+;(async () => {
+  const status = await git.status()
+  if (status.modified.length === 0) return
+  await git.add("./*")
+  await git.commit(`gitrt-auto-commit on ${new Date()}`)
+  console.log("status", status)
+})()
+
+const run = (_settings) => {
+  Object.assign(settings, _settings)
+  console.log("ttt", settings, _settings)
+}
+
+run({ timeout: 999 })
+
+module.exports = { run }
