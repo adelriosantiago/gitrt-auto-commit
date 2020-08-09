2ae089f4c1b74c0c2d88cf297a2c4ace958677b0
Alejandro del Río <adelriosantiago@gmail.com>
2020-08-09 01:47:29 -0500

    add timeout

--- index.js
+++ index.js
@@ -5,27 +5,39 @@ const path = require("path")
 const simpleGit = require("simple-git")
 
 let settings = {
-  baseDir: path.join(process.cwd(), "./"),
+  // SimpleGIT settings
   binary: "git",
   maxConcurrentProcesses: 6,
+  // GitRT settings
+  folder: [],
   timeout: 1000,
+  verbose: false,
 }
-
-const git = simpleGit(settings)
-
-;(async () => {
-  const status = await git.status()
-  if (status.modified.length === 0) return
-  await git.add("./*")
-  await git.commit(`gitrt-auto-commit on ${new Date()}`)
-  console.log("status", status)
-})()
+let timer
 
 const run = (_settings) => {
   Object.assign(settings, _settings)
-  console.log("ttt", settings, _settings)
+  settings.baseDir = path.join(process.cwd(), ..._settings.folder)
+
+  const git = simpleGit(settings)
+
+  timer = setTimeout(async function myTimer() {
+    const status = await git.status()
+
+    if (status.modified.length !== 0) {
+      await git.add("./*")
+      await git.commit(`gitrt-auto-commit on ${new Date()}`)
+      if (settings.verbose) console.log("Git commit done")
+    }
+    timer = setTimeout(myTimer, settings.timeout)
+  }, settings.timeout)
 }
 
-run({ timeout: 999 })
+const stop = () => {
+  clearTimeout(timer)
+}
 
-module.exports = { run }
+// TEST
+run({ folder: ["example-repo"], timeout: 1000, verbose: true })
+
+module.exports = { run, stop }
